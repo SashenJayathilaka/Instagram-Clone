e7177dfb507aedb3b869762e249f7cb3391f8569
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2022-10-12 21:20:50 +0530

    🐛 Bug Fixes(#16)

--- firebase.js
+++ firebase.js
@@ -6,13 +6,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyARXDiYjznCzi26fvt00cEXDp-88f6zNTc",
-  authDomain: "messenger-mern-clone-dca82.firebaseapp.com",
-  projectId: "messenger-mern-clone-dca82",
-  storageBucket: "messenger-mern-clone-dca82.appspot.com",
-  messagingSenderId: "468616337977",
-  appId: "1:468616337977:web:1e80cf727273ca38288be8",
-  measurementId: "G-R8YCSSJ33J",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
