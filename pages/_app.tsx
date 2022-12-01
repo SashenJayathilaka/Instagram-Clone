d241ca88e6321c20854aa2994c3c22544a8fd173
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2022-12-01 10:32:58 +0530

    🐛 Bug Fixes (#37)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
